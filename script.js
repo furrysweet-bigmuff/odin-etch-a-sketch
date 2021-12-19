060db62827e9dc3eab5690137e527cf5ce541e04
furrysweet-bigmuff <greyhotel2@gmail.com>
2021-12-19 21:05:20 +0200

    Finished skeleton of project, added bg image functionality

---
+++ script.js
@@ -0,0 +1,47 @@
+const container = document.getElementById('container');
+const btn = document.querySelector('button');
+const containerWidth = container.offsetWidth;
+
+function hover() {
+    this.classList.add('hover');
+}
+
+function removeHover() {
+    this.classList.remove('hover');
+}
+
+function createDivs(num) {
+    let bgHorizontalPosition;
+    let bgVerticalPosition;
+    // высчитываем bg position для вертикального значения
+    for (let i = 0; i < num; i++) {
+        bgHorizontalPosition = 0;
+        bgVerticalPosition = (containerWidth / num) * i;
+        // высчитываем bg position для горизонтального значения
+        for (let n = 0; n < num; n++) {
+            bgHorizontalPosition = (containerWidth / num) * n;
+            let newDiv = document.createElement('div');
+            newDiv.style.cssText = 'width: ' + containerWidth / num + 'px; height: ' + containerWidth / num + 
+            'px; background-position: -' + bgHorizontalPosition + 'px -' + bgVerticalPosition + 'px;';
+            container.appendChild(newDiv);
+        }
+    }
+    let divs = document.querySelectorAll('#container div');
+
+    divs.forEach(div => div.addEventListener('mouseover', hover));
+    divs.forEach(div => div.addEventListener('mouseout', removeHover));
+}
+
+function restart() {
+    container.replaceChildren();
+    let num = prompt('Enter value below 100')
+    createDivs(num);
+}
+
+btn.addEventListener('click', restart);
+
+createDivs(16);
+
+
+
+
