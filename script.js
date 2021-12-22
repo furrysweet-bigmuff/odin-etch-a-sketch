18d0371783a49861fa7510cd7c4670bca51c16b4
furrysweet-bigmuff <greyhotel2@gmail.com>
2021-12-22 12:20:45 +0200

    finished project

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const rangeSlider = document.getElementById('range');
 const rangeLabel = document.getElementById('rangeLabel');
 const btnReset = document.getElementById('reset');
 const btnFile = document.getElementById('file');
+const fileTrigger = document.getElementById('fileTrigger');
 const containerWidth = container.offsetWidth;
-let image = document.getElementById('image');
+let imageUrl;
 let currentColor = '#000';
 let randomise = false;
 
@@ -16,7 +17,14 @@ function hover() {
 }
 
 function imageHover() {
-    randomise ? this.style.backgroundColor = getRandomColor() : this.style.backgroundColor = currentColor;
+    this.classList.add('hover');
+}
+
+function checkImageBg() {
+    let checker = document.getElementsByClassName('imageBg').length;
+    if (checker == 0) return
+    container.classList.remove('imageBg');
+    createDivs(rangeSlider.value);
 }
 
 function getRandomColor(){
@@ -30,6 +38,7 @@ function getRandomColor(){
 
 function createImageDivs(num, bgImg) {
     container.replaceChildren();
+    container.classList.add('imageBg');
     let bgHorizontalPosition;
     let bgVerticalPosition;
     // высчитываем bg position для вертикального значения
@@ -64,30 +73,41 @@ function createDivs(num) {
 }
 
 btnRainbow.addEventListener('click', function() {
+    checkImageBg()
     randomise = true;
 });
 btnBlack.addEventListener('click', function() {
+    checkImageBg()
     randomise = false;
     currentColor = '#000';
 });
 btnPicker.addEventListener('input', function() {
+    checkImageBg()
     randomise = false;
     currentColor = this.value;
 });
 btnReset.addEventListener('click', function() {
+    checkImageBg()
     let divs = document.querySelectorAll('#container div');
     divs.forEach(div => div.style.backgroundColor = "white");
+    currentColor = '#000';
 });
 rangeSlider.addEventListener('input', function() {
     rangeLabel.textContent = this.value + "x" + this.value;
-    createDivs(this.value);
+    if (document.getElementsByClassName('imageBg').length == 0) {
+        createDivs(this.value);
+    } else {
+        createImageDivs(this.value, imageUrl);
+    }
 });
-
 btnFile.addEventListener('change', function(event) {
     currentColor = '#000';
     randomise = false;
-    let imageUrl = URL.createObjectURL(event.target.files[0]);
-    createImageDivs(16, imageUrl);
+    imageUrl = URL.createObjectURL(event.target.files[0]);
+    createImageDivs(rangeSlider.value, imageUrl);
+});
+fileTrigger.addEventListener('click', function() {
+    btnFile.click()
 });
 
 createDivs(16);
